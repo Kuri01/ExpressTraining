8c216f267e4e6727461f7c0731cf49ab0da2554b
Kuri01 <kamil.kuriata01@gmail.com>
2022-02-03 13:10:03 +0100

    21.3 finish

--- server.js
+++ server.js
@@ -3,35 +3,42 @@ const path = require('path');
 const hbs = require('express-handlebars');
 
 const app = express();
-// app.engine('.hbs', hbs());
-// app.set('view engine', '.hbs');
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
+app.engine(
+  'hbs',
+  hbs({ extname: 'hbs', layoutsDir: './views/layouts', defaultLayout: 'main' })
+);
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+  res.render('home');
 });
 
-app.use('/user', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '/views/forbidden.html'));
-});
-
-app.get('/home', (req, res) => {
-  res.show('home.html');
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
 });
 
 app.use((req, res) => {
-  res.status(404).show('notFound.jpg');
+  res.status(404).send('404 not found...');
 });
 
-// app.get('/hello/:name', (req, res) => {
-//   res.render('hello', { layout: false, name: req.params.name });
-// });
-
 app.listen(8000, () => {
-  console.log('server is running on port 8000');
+  console.log('Server is running on port: 8000');
 });
